e7582ebea03233893d3b882e92f4f12b4a31d2d1
Sal Tardibuono <s.tardibuono@live.com>
2017-02-18 15:21:16 -0800

    added javascript functions allowing users to specify grid size

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+
 // initializing functions
 
 function init(){
@@ -16,3 +18,51 @@ window.onload = init;
 var pairsLeft = document.getElementById("pairs-left");
 var matching = document.getElementById("matching");
 var message = document.getElementById("message");
+
+*/
+
+
+function createCols(gridSize){
+  for ( var i=1; i<gridSize+1; i++){
+    var col = document.createElement("div");
+    col.className = "col";
+    col.setAttribute('id', 'col-'+ i);
+    document.getElementById("card-container").appendChild(col);
+  }
+}
+
+function createCards(gridSize){
+
+  var cardCount = 1;
+
+  // loop through each row
+  for ( var i=1; i<gridSize+1; i++){
+
+    var colSelect = document.getElementById("col-"+i);
+    var colNumber = i;
+
+    // loop through each card
+    for (var j=1; j<gridSize+1; j++){
+
+      var cardBack = document.createElement('div');
+      cardBack.className = "card";
+      cardBack.setAttribute('id', "card-"+(cardCount)+'-back');
+      colSelect.appendChild(cardBack);
+      cardCount++;
+    }
+
+  }
+
+}
+
+
+function createGrid(){
+
+  var select = document.getElementById('grid-type');
+  var gridValue = parseInt(select.value);
+
+  createCols(gridValue);
+  createCards(gridValue);
+
+  select.style.display = 'none'; // hide select box
+}
