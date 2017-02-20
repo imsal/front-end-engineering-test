0bc98893ee60119904b68d753af33310f0d76b28
Sal Tardibuono <s.tardibuono@live.com>
2017-02-20 13:08:54 -0800

    fixed bug where player could select already matched cards and fixed bug where player could select the same card twice.

--- script.js
+++ script.js
@@ -23,133 +23,152 @@ var blastoise = {
 name: 'Blastoise',
 url: "assets/images/blastoise.png",
 mp3: "assets/sounds/blastoise.mp3",
-ogg: "assets/sounds/blastoise.ogg"
+ogg: "assets/sounds/blastoise.ogg",
+matched: false
 };
 
 var charizard = {
 name: 'Charizard',
 url: "assets/images/charizard.png",
 mp3: "assets/sounds/charizard.mp3",
-ogg: "assets/sounds/charizard.ogg"
+ogg: "assets/sounds/charizard.ogg",
+matched: false
 };
 
 var gengar = {
 name: 'Gengar',
 url: "assets/images/gengar.png",
 mp3: "assets/sounds/gengar.mp3",
-ogg: "assets/sounds/gengar.ogg"
+ogg: "assets/sounds/gengar.ogg",
+matched: false
 };
 
 var lugia = {
 name: 'Lugia',
 url: "assets/images/lugia.png",
 mp3: "assets/sounds/lugia.mp3",
-ogg: "assets/sounds/lugia.ogg"
+ogg: "assets/sounds/lugia.ogg",
+matched: false
 };
 
 var machamp = {
 name: 'Machamp',
 url: "assets/images/machamp.png",
 mp3: "assets/sounds/machamp.mp3",
-ogg: "assets/sounds/machamp.ogg"
+ogg: "assets/sounds/machamp.ogg",
+matched: false
 };
 
 var mewtwo = {
 name: 'Mewtwo',
 url: "assets/images/mewtwo.png",
 mp3: "assets/sounds/mewtwo.mp3",
-ogg: "assets/sounds/mewtwo.ogg"
+ogg: "assets/sounds/mewtwo.ogg",
+matched: false
 };
 
 var moltres = {
 name: 'Moltres',
 url: "assets/images/moltres.png",
 mp3: "assets/sounds/moltres.mp3",
-ogg: "assets/sounds/moltres.ogg"
+ogg: "assets/sounds/moltres.ogg",
+matched: false
 };
 
 var pikachu = {
 name: 'Pikachu',
 url: "assets/images/pikachu.png",
 mp3: "assets/sounds/pikachu.mp3",
-ogg: "assets/sounds/pikachu.ogg"
+ogg: "assets/sounds/pikachu.ogg",
+matched: false
 };
 
 var pinsur = {
 name: 'Pinsur',
 url: "assets/images/pinsur.png",
 mp3: "assets/sounds/pinsur.mp3",
-ogg: "assets/sounds/pinsur.ogg"
+ogg: "assets/sounds/pinsur.ogg",
+matched: false
 };
 
 var venusaur = {
 name: 'Venusaur',
 url: "assets/images/venusaur.png",
 mp3: "assets/sounds/venusaur.mp3",
-ogg: "assets/sounds/venusaur.ogg"
+ogg: "assets/sounds/venusaur.ogg",
+matched: false
 };
 
 var alakazam = {
 name: 'Alakazam',
 url: "assets/images/alakazam.png",
 mp3: "assets/sounds/alakazam.mp3",
-ogg: "assets/sounds/alakazam.ogg"
+ogg: "assets/sounds/alakazam.ogg",
+matched: false
 };
 
 var geodude = {
 name: 'Geodude',
 url: "assets/images/geodude.png",
 mp3: "assets/sounds/geodude.mp3",
-ogg: "assets/sounds/geodude.ogg"
+ogg: "assets/sounds/geodude.ogg",
+matched: false
 };
 
 var golduck = {
 name: 'Golduck',
 url: "assets/images/golduck.png",
 mp3: "assets/sounds/golduck.mp3",
-ogg: "assets/sounds/golduck.ogg"
+ogg: "assets/sounds/golduck.ogg",
+matched: false
 };
 
 var ninetales = {
 name: 'Ninetales',
 url: "assets/images/ninetales.png",
 mp3: "assets/sounds/ninetales.mp3",
-ogg: "assets/sounds/ninetales.ogg"
+ogg: "assets/sounds/ninetales.ogg",
+matched: false
 };
 
 var staryu = {
 name: 'Staryu',
 url: "assets/images/staryu.png",
 mp3: "assets/sounds/staryu.mp3",
-ogg: "assets/sounds/staryu.ogg"
+ogg: "assets/sounds/staryu.ogg",
+matched: false
 };
 
 var dugtrio = {
 name: 'Dugtrio',
 url: "assets/images/dugtrio.png",
 mp3: "assets/sounds/dugtrio.mp3",
-ogg: "assets/sounds/dugtrio.ogg"
+ogg: "assets/sounds/dugtrio.ogg",
+matched: false
 };
 
 var ditto = {
 name: 'Ditto',
 url: "assets/images/ditto.png",
 mp3: "assets/sounds/ditto.mp3",
-ogg: "assets/sounds/ditto.ogg"
+ogg: "assets/sounds/ditto.ogg",
+matched: false
 };
 
 var raikou = {
 name: 'Raikou',
 url: "assets/images/raikou.png",
 mp3: "assets/sounds/raikou.mp3",
-ogg: "assets/sounds/raikou.ogg"
+ogg: "assets/sounds/raikou.ogg",
+matched: false
 };
 
 var vaporeon = {
 name: 'Vaporeon',
 url: "assets/images/vaporeon.png",
 mp3: "assets/sounds/vaporeon.mp3",
-ogg: "assets/sounds/vaporeon.ogg"
+ogg: "assets/sounds/vaporeon.ogg",
+matched: false
 };
 
 var cardObjects = [blastoise, charizard, gengar, lugia, machamp, mewtwo, moltres, pikachu, pinsur, venusaur, alakazam, geodude, golduck, ninetales, staryu, dugtrio, ditto, raikou, vaporeon];
@@ -251,6 +270,9 @@ function gameCompletion(){
   var newTitle = document.createElement('h1');
   newTitle.innerHTML = 'Congratulations, you have completed the game!';
   document.getElementById('container').appendChild(newTitle);
+  var newParagraph = document.createElement('p');
+  newParagraph.innerHTML = 'It only took you ' + attempts + ' attempts!';
+  document.getElementById('container').appendChild(newParagraph);
   // play victory sound
   var mp3Url = "assets/sounds/victory_music.mp3";
   var oggUrl = "assets/sounds/victory_music.ogg";
@@ -263,11 +285,13 @@ function gameCompletion(){
   audio.play();
 }
 
-function evaluate(){
+function evaluate(clickedID){
   var firstPick = document.getElementById("first-pick");
   var secondPick = document.getElementById("second-pick");
 
   if (cardOne.name == cardTwo.name){
+    // correct
+    cardObjects[clickedID-1].matched = true;
     pairsLeft -= 1;
     document.getElementById("pairs-left").innerHTML = pairsLeft;
 
@@ -293,39 +317,56 @@ function evaluate(){
   // check if player completed the game
   if (pairsLeft == 0){
     // something
-    gameCompletion();
+    setTimeout(function(){
+      gameCompletion();
+      }, 1000);
+
   }
+
+}
+
+function checkIfCardsHaveBeenMatched(){
 
 }
 
 
 
 function clickCard(clickedID){
-
+  // errors -- player clicks an already matched card
+  if (cardObjects[clickedID-1].matched == true) {
+    alert('This card has already been matched! Please select a new card!');
+  } else {
   // first card
   if (numberOfClicks%2 == 1){
-  document.getElementById('matching-2').innerHTML = '';
-  document.getElementById("first-pick").className = '';
-  document.getElementById("second-pick").className = '';
-  var firstCardText = document.getElementById('matching-1');
-  cardOne.name = flipCard(firstCardText, clickedID);
-  playSound(clickedID);
-  numberOfClicks += 1;
-  cardOne.id = clickedID;
+  // Player clicks a card that has already been matched.
+        document.getElementById('matching-2').innerHTML = '';
+        document.getElementById("first-pick").className = '';
+        document.getElementById("second-pick").className = '';
+        var firstCardText = document.getElementById('matching-1');
+        cardOne.name = flipCard(firstCardText, clickedID);
+        playSound(clickedID);
+        numberOfClicks += 1;
+        cardOne.id = clickedID;
+
 
   } else {
-  // put some code that doesn't allow the user to pick the same first card. ////////////////
   // second card
-  var secondCardText = document.getElementById('matching-2');
-  cardTwo.name = flipCard(secondCardText, clickedID);
-  playSound(clickedID);
-  numberOfClicks += 1;
-  cardTwo.id = clickedID;
-  attempts += 1;
-  document.getElementById('attempts').innerHTML = attempts;
-  evaluate();
-  }
+  if (clickedID == cardOne.id){
+    // play clicks the same card twice.
+    alert('Please select a different card!');
+  } else {
+    // put some code that doesn't allow the user to pick the same first card. ////////////////
+    var secondCardText = document.getElementById('matching-2');
+    cardTwo.name = flipCard(secondCardText, clickedID);
+    playSound(clickedID);
+    numberOfClicks += 1;
+    cardTwo.id = clickedID;
+    attempts += 1;
+    evaluate(clickedID);
+    }
 
+    }
+  }
 }
 
 
